aea963d4d1abbad07cdc472b8b5d207c1160ae6c
lovejot-1234 <lovejotkaur.mardon@gmail.com>
2025-08-12 14:59:35 +0530

    Added multiple files

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import react from "react";
+import { RiEdit2Fill } from "react-icons/ri";
+import { RiDeleteBin6Line } from "react-icons/ri";
+const Button = () => {
+return <div>
+    <RiEdit2Fill />  <RiDeleteBin6Line /> 
+  </div>
+};
+export default Button;
